973f2bcde530a7a62667f1d71852434bb13288d8
AfrinRabik <afrinrabikm@gmail.com>
2026-09-24 21:56:30 +0530

    Time: 1 ms (99.69%) | Memory: 45.7 MB (40.74%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -5,26 +5,20 @@ class Solution
         for(int i=0;i<nums.length;i++)
         { 
             int temp=nums[i];
-            if(i==temp&&i<=9)
+            int r=0;
+            int sum=0;
+            while(temp!=0)
             {
-                return i;
-            }
-            else if(temp>9)
-            {
-                int r=0;
-                int sum=0;
-                while(temp!=0)
-                {
-                    r=temp%10;
-                    sum+=r;
-                    temp=temp/10;
+                r=temp%10;
+                sum+=r;
+                temp=temp/10;
 
-                }
-                if(sum==i)
-                {
-                    return i;
-                }
             }
+             if(sum==i)
+             {
+                return i;
+             }
+            
         }
         return -1;
     }
