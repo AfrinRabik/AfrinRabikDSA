cd6d20319f3c6a06aaa8413bfdafaeec693b52f5
AfrinRabik <afrinrabikm@gmail.com>
2026-09-24 22:58:04 +0530

    Time: 2 ms (97.89%) | Memory: 43.3 MB (57.31%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public boolean isValid(String s) 
+    {
+        Stack<Character> stack=new Stack();
+        for(char c:s.toCharArray())
+        {
+            
+            if(c=='('||c=='{'||c=='[')
+            {
+                stack.push(c);
+            }
+            else
+            {
+                if(stack.isEmpty())
+                {
+                    return false;
+                }
+                else
+                {
+                    char t=stack.pop();
+                    if((c==')'&&t!='(')||(c=='}'&&t!='{')||(c==']'&&t!='['))
+                    {
+                        return false;
+                    }
+                    
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
